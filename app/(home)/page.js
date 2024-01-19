f8063adb40957f2312e2d29fb16120dfe1a2bb8f
Anubhav Pal <anubhavpal15june@gmail.com>
2024-01-19 23:24:42 +0530

    Create Files and Folder Structure

---
+++ app/(home)/page.js
@@ -0,0 +1,17 @@
+import HeroSection from "@/components/HeroSection";
+
+export default function Page() {
+  return (
+    <div className="main-home-container p-96">
+      <div className="p-96">
+        <HeroSection />
+        <HeroSection />
+        <HeroSection />
+        <HeroSection />
+        <HeroSection />
+        <HeroSection />
+        <HeroSection />
+      </div>
+    </div>
+  )
+}
