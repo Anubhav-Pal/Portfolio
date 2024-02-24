ca7f691d6d8f9a2d3b901d5b689d4854cf8a7d1e
Anubhav Pal <anubhavpal15june@gmail.com>
2024-02-24 09:17:08 +0530

    Update Work Experience

--- components/Work.jsx
+++ components/Work.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 
 const companies = [
   {
+    position: "Frontend Software Engineering Intern",
+    companyName: "ClientFlowX",
+    companyWebsite: "clientflowx.com",
+    companyWebsiteLink: "https://www.clientflowx.com/",
+    workedOn: [],
+    startingMonth: "March, 2024",
+    endingMonth: "present",
+  },
+  {
     position: "Software Engineering Intern",
     companyName: "Wonder Learn Games",
     companyWebsite: "",
     companyWebsiteLink: "",
     workedOn: ["Developed engaging user interfaces and interactive components using React for a teen learning application, ensuring seamless navigation and user experience.",
-  "Learned about tools and technologies such as ReactNative and Native Features."],
+      "Learned about tools and technologies such as ReactNative and Native Features."],
     startingMonth: "February, 2024",
-    endingMonth: "present",
+    endingMonth: "",
   },
   {
     position: "Frontend Web Developer Intern",
@@ -18,7 +27,7 @@ const companies = [
     companyWebsiteLink: "http://www.altruismlabs.com/",
     workedOn: ["Designed and developed sophisticated interactive React components for seamless web and mobile experiences.", "Successfully integrated the Chart.js library to enhance data visualization capabilities."],
     startingMonth: "December, 2023",
-    endingMonth: "present",
+    endingMonth: "February, 2024",
   },
 
 ]
@@ -28,19 +37,19 @@ const Work = () => {
     <div className='flex flex-col gap-8'>
       {companies.map((company, index) => ( // Added index parameter for unique key
         <div className='border-l-2 pl-6 hover:border-orange-300 cursor-default transition-all flex flex-col gap-5' key={index}> {/* Added key prop */}
-          <div className='flex flex-col gap-2'>
+          <div className='flex sm:flex-row flex-col items-start justify-between gap-2'>
             <div>
-              <div className='capitalize text-lg'>{company.position} , {company.companyName}</div>
-              {company.companyName==="Wonder Learn Games"?'':
-              <div className={`text-sm `}>at, <a href={company.companyWebsiteLink}>{company.companyWebsite}</a></div>
+              <div className='capitalize text-md '>{company.position}, {company.companyName}</div>
+              {company.companyName === "Wonder Learn Games" ? '' :
+                <div className={`text-sm `}>at, <a href={company.companyWebsiteLink}>{company.companyWebsite}</a></div>
               }
             </div>
-            <div className='text-sd text-sm'>{company.startingMonth}-{company.endingMonth}</div>
+            <div className='text-sd text-sm'>{company.startingMonth} <span className={`${company.endingMonth === "" ? 'hidden' : ''}`}>-</span>  {company.endingMonth}</div>
           </div>
           <div>
             <ul className='list-disc flex flex-col gap-3 list-inside'>
               {
-                company.workedOn.map((thingWorkedOn, idx) => ( 
+                company.workedOn.map((thingWorkedOn, idx) => (
                   <li className='text-sd text-sm' key={idx}>{thingWorkedOn}</li>
                 ))
               }
