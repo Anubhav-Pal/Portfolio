1ff4dc9e20689d6add47bb1ea109e565170b8749
Anubhav Pal <anubhavpal.excl@gmail.com>
2024-09-21 18:14:40 +0530

    Deploy static site

---
+++ out/assets/assets.js
@@ -0,0 +1,52 @@
+import bar from "./bar.png";
+import Bng from "./Bng.png";
+import clouds from "./clouds.png";
+import lotus from "./lotus.png";
+import plane from "./plane.png";
+import planeout from "./planeout.png";
+import qutub from "./qutub.png";
+import redbrick from "./redbrick.png";
+import sft from "./sft.png";
+import sunset from "./sunset.png";
+import vase from "./vase.png";
+import online from "./online.json";
+
+import dropdown from "./dropdown.png";
+import abitco from "./abitco.png";
+import chatbot from "./chatbot.png";
+import portfolio from "./portfolio.png";
+import dashboard from "./dashboard.png";
+import punjab from "./punjab.png";
+import appointly from "./appointly.png";
+import gpt3page from "./gpt3page.png";
+import modernbank from "./modernbank.png";
+import usabilityhub from "./usabilityhub.png";
+import timeline from "./timeline.png";
+import deck from "./deck.png";
+
+export {
+  bar,
+  Bng,
+  clouds,
+  lotus,
+  deck,
+  plane,
+  planeout,
+  qutub,
+  redbrick,
+  timeline,
+  sft,
+  sunset,
+  appointly,
+  vase,
+  online,
+  dropdown,
+  abitco,
+  chatbot,
+  portfolio,
+  dashboard,
+  punjab,
+  gpt3page,
+  modernbank,
+  usabilityhub,
+};
